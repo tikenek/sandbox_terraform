ca73affdbea3c027b0545a74e78b1eef9717f2bf
Tynar Esenbaeva <esenbaevat@yahoo.com>
2023-03-07 22:19:35 -0500

    update nat

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,7 +14,8 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = "${aws_subnet.public_subnets.*.id}"
+  # subnet_id     = "${aws_subnet.public_subnets.*.id}"
+  subnet_id     = element(aws_subnet.public_subnets.*.id, 0)
   depends_on    = [aws_internet_gateway.gw]
 }
 
